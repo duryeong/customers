d948985202a3dc06323a393e24af623c2c57a21a
Your Name <duryeong@gmail.com>
2020-08-16 22:04:36 +0900

    test remove

--- src/test/kotlin/com/microservices/chapter11/CustomerControllerTest.kt
+++ src/test/kotlin/com/microservices/chapter11/CustomerControllerTest.kt
@@ -1,5 +1,6 @@
-package com.microservices.chapter12
+package com.microservices.chapter11
 
+import org.junit.Assert.*
 import com.microservices.chapter11.CustomerController
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -8,39 +9,42 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest
 class CustomerControllerTest {
-  @Autowired
-  lateinit var mockMvc: MockMvc
+    @Autowired
+    lateinit var mockMvc: MockMvc
 
-  @Test
-  fun `we should get the customer list`() {
-    mockMvc.perform(get("/customers"))
-        .andExpect(status().isOk)
-        .andExpect(jsonPath("\$[0].id").value(1))
-        .andExpect(jsonPath("\$[0].name").value("Kotlin"))
-        .andExpect(jsonPath("\$[1].id").value(2))
-        .andExpect(jsonPath("\$[1].name").value("Spring"))
-        .andExpect(jsonPath("\$[2].id").value(3))
-        .andExpect(jsonPath("\$[2].name").value("Microservice"))
-        .andExpect(jsonPath("\$[3].id").value(4))
-        .andExpect(jsonPath("\$[3].name").value("OpenShift"))
-        .andDo(print())
-  }
+    @Test
+    fun `we should get the customer list`() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/customers"))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[0].name").value("Kotlin"))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[1].name").value("Spring"))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[2].id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[2].name").value("Microservice"))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[3].id").value(4))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$[3].name").value("OpenShift"))
+                .andDo(MockMvcResultHandlers.print())
+    }
 
-  @Test
-  fun `we should get a customer by id`() {
-    mockMvc.perform(get("/customer/1"))
-        .andExpect(status().isOk)
-        .andExpect(jsonPath("\$.id").value(1))
-        .andExpect(jsonPath("\$.name").value("Kotlin"))
-        .andDo(print())
-  }
+    @Test
+    fun `we should get a customer by id`() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/customer/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("\$.id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("\$.name").value("Kotlin"))
+                .andDo(MockMvcResultHandlers.print())
+    }
 }
